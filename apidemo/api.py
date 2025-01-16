4bcea0db09aa21d70a3a4ecb92393ed69a2044ad
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-15 21:22:09 -0300

    Updating api.py

--- apidemo/api.py
+++ apidemo/api.py
@@ -3,5 +3,9 @@ from ninja import NinjaAPI
 api = NinjaAPI()
 
 @api.get("/hello")
-def hello(request):
-  return {"message": "Hello World"}
+def hello(request, name):
+  return {"message": f"Hello {name}"}
+
+@api.get("/math/{a}and{b}")
+def math(request, a:int, b:int):
+  return {"add": a + b, "multiply": a * b}
