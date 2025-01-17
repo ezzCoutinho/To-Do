906249236f8b8c0237519d82931862a814f7b851
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-17 20:06:20 -0300

    Adding new route

--- apidemo/api.py
+++ apidemo/api.py
@@ -1,30 +1,12 @@
 from ninja import NinjaAPI, Schema
-from schemas import HelloSchema, UserSchema, Error, Employee
-
+from apidemo.schemas import EmployeeIn
+from apidemo.models import Employee
 api = NinjaAPI()
 
 @api.post("/employees")
-def create_employee(request, payload: EmployeeSchema):
+def create_employee(request, payload: EmployeeIn):
     employee = Employee.objects.create(**payload.dict())
     return {"id": employee.id}
-
-@api.get("/me", response={200: UserSchema, 403: Error}) # Primeira schema com rotas.
-def me(request):
-  if request.user.is_authenticated:
-    return {
-    "username": "ezzcoutinho",
-    "is_authenticated": True,
-    "email": "lammerlopes@hotmail.com",
-    "first_name": "Ezequiel",
-    "last_name": "Coutinho"
-    }
-  return 403, {"message": "Not authenticated"}
-
-
-
-# @api.post("/hello")
-# def post_hello(request, data: HelloSchema):
-#   return {"message": f"Hello {data.name}"}
 
 @api.get("/hello") # primeira rota
 def hello(request, name):
