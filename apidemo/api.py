c23be0fe82468f3f8d295b0dab0ec452a63e2dba
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-17 21:43:41 -0300

    Adding new employee with upload file

--- apidemo/api.py
+++ apidemo/api.py
@@ -1,12 +1,15 @@
-from ninja import NinjaAPI, Schema
+from ninja import NinjaAPI, Schema, UploadedFile, File, Form
 from apidemo.schemas import EmployeeIn
 from apidemo.models import Employee
 api = NinjaAPI()
 
 @api.post("/employees")
-def create_employee(request, payload: EmployeeIn):
-    employee = Employee.objects.create(**payload.dict())
-    return {"id": employee.id}
+def create_employee(request, employee: EmployeeIn = Form(...), cv: UploadedFile = File(...)):
+    employee_dict = employee.dict()
+    employee_instance = Employee(**employee_dict)
+    employee_instance.cv.save(cv.name, cv)
+    employee_instance.save()
+    return {"id": employee_instance.id}
 
 @api.get("/hello") # primeira rota
 def hello(request, name):
