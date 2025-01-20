c8c603d1e5de9e4ac6f730a15cd60c9cc7cc897b
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-19 21:48:03 -0300

    New settings cors

--- apidemo/apidemo/settings.py
+++ apidemo/apidemo/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apidemo'
+    'apidemo',
+    'corsheaders',
+
     
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Adicione o endereço do seu frontend Next.js
+]
