414ff795eb8d436d1bc2fa6c3e3386654228e4a2
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-23 22:18:14 -0300

    Updating home

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,18 @@
 import Link from 'next/link';
+import React from 'react';
+import TestAuthComponent from '../app/tests/TestAuthComponent';
 
-export default function Home() {
+
+const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Projeto to-do</h1>
       <Link href="/login">Login</Link>
       <Link href="/register">Register</Link>
+      <h1>Página Inicial</h1>
+      <TestAuthComponent/>
     </div>
   );
 }
+
+export default HomePage;
