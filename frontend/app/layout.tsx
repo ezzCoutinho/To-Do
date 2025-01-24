fab94e6a6d951cdb898557465149537674250fe5
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-23 23:12:23 -0300

    updating

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,13 +1,21 @@
-import "../styles/global.css";
+import { ThemeProvider } from "@/components/theme-provider"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  )
 }
