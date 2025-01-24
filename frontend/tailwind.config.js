99f93628cf4ab0d2d573b55f51b6eddd450ace67
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-23 23:12:18 -0300

    updating

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: { warning: "hsl(var(--warning))",
+        "warning-foreground": "hsl(var(--warning-foreground))",}
+    },
   },
   plugins: [],
 };
