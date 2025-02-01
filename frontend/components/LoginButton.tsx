597b7baf4bf6b5a3b50f37527b0489ab3c79e95c
ezzCoutinho <lammerlopes@hotmail.com>
2025-02-01 00:35:13 -0300

    Add LoginButton component for GitHub authentication

---
+++ frontend/components/LoginButton.tsx
@@ -0,0 +1,10 @@
+// filepath: /components/LoginButton.tsx
+import { signIn } from "next-auth/react";
+
+export default function LoginButton() {
+  return (
+    <button onClick={() => signIn("github")}>
+      Login with GitHub
+    </button>
+  );
+}
