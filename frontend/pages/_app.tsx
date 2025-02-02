8e36602285653d7ed71d3032bf12ba24f1939b92
ezzCoutinho <lammerlopes@hotmail.com>
2025-02-02 11:59:14 -0300

    Add global CSS import for consistent styling across the application

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
