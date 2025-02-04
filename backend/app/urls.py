9b3ca7969e992133fd92fc0bed18c076daf3370b
ezzCoutinho <lammerlopes@hotmail.com>
2025-02-03 21:03:51 -0300

    Add NinjaAPI integration and configure router for API endpoints

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .api import api
+from .api import api, router
 from django.urls import path
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+api.add_router("/api/", router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
