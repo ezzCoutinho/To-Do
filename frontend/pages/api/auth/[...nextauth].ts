34a5bc735c4f53646a992457bd41b6c6c49aeae0
ezzCoutinho <lammerlopes@hotmail.com>
2025-02-04 22:09:35 -0300

    Add Google and Credentials providers to NextAuth for enhanced authentication options

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
   providers: [
@@ -7,13 +9,47 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // Adicione outros providers aqui
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const res = await fetch("http://127.0.0.1:8000/api/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(credentials),
+        });
+
+        const user = await res.json();
+
+        if (res.ok && user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
     async session({ session, token }) {
-      session.user.id = token.sub; // Exemplo: adicionar ID do usuário à sessão
+      session.user.id = token.id; // Exemplo: adicionar ID do usuário à sessão
       return session;
-    }
-  }
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl + "/tarefas";
+    },
+  },
 });
