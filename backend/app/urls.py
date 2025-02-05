bfd3f15e532e5d75d18cd0443f3d1bb7fc597a60
ezzCoutinho <lammerlopes@hotmail.com>
2025-02-04 21:08:05 -0300

    Refactor URL configuration to streamline API imports and remove unused code

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from .api import api, router
 from django.urls import path
-from ninja import NinjaAPI
-
-api = NinjaAPI()
-api.add_router("/api/", router)
+from .api import api  # Certifique-se de importar de api.py
 
 urlpatterns = [
     path('admin/', admin.site.urls),
