3cf78532d2f3323c145f062512ed1967ae9e9bc2
ezzCoutinho <lammerlopes@hotmail.com>
2025-02-06 22:43:17 -0300

    Add WebSocket support to ASGI configuration

--- backend/app/asgi.py
+++ backend/app/asgi.py
@@ -1,6 +1,11 @@
 import os
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from app.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(websocket_urlpatterns),  # WebSocket configurado corretamente
+})
