48b8a46ac55f831a6a7c07cbf7e9ebf6e4ebb288
ezzCoutinho <lammerlopes@hotmail.com>
2025-02-09 23:12:51 -0300

    Clean up formatting in routing.py for WebSocket URL patterns

--- backend/app/routing.py
+++ backend/app/routing.py
@@ -1,6 +1,8 @@
+# app/routing.py
+
 from django.urls import path
 from app.consumers import TarefaConsumer
 
 websocket_urlpatterns = [
-    path("ws/tarefas/", TarefaConsumer.as_asgi()),  # WebSocket para tarefas
+    path('ws/tarefas/', TarefaConsumer.as_asgi()),
 ]
