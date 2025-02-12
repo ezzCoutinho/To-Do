9bca384767017404bf14408ec16b20067f9262cb
ezzCoutinho <lammerlopes@hotmail.com>
2025-02-12 20:54:45 -0300

    Refactor ASGI configuration to use websocket URL patterns for improved routing

--- backend/app/asgi.py
+++ backend/app/asgi.py
@@ -1,14 +1,14 @@
+import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from django.core.asgi import get_asgi_application
-from app.consumers import TarefaConsumer  # Ajuste para o nome correto
+from .routing import websocket_urlpatterns  # Ajuste conforme o nome do seu app
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path("ws/tarefas/", TarefaConsumer.as_asgi()),  # Verifique a URL
-        ])
+        URLRouter(websocket_urlpatterns)
     ),
 })
