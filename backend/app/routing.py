6c75226b9fb8f1da0aa0c2d30a158804767d5d0a
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-21 22:37:34 -0300

    Remover arquivos do backend e frontend, incluindo configurações, componentes e páginas do Next.js, além de arquivos de dependências e configuração do projeto.

--- backend/app/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from .consumers import TarefaConsumer  # Verifique se o caminho está correto!
-
-websocket_urlpatterns = [
-    re_path(r"ws/tarefas/$", TarefaConsumer.as_asgi()),
-]
--- backend/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Tarefa  # Importe o modelo Tarefa
-
-class TarefaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tarefa
-        fields = '__all__'  # Ou defina os campos específicos que você quer incluir
