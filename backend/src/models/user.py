08b13d3cc5b3e0ac2a8805cfc005d93165b260e3
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-23 21:04:16 -0300

    Atualizar modelo de usuário para renomear campo 'nome' para 'username' e corrigir mensagem de erro para incluir ponto final.

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -4,14 +4,12 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError("O email é obrigatório")
+            raise ValueError("O email é obrigatório.")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -25,16 +23,16 @@ class UserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    nome = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["nome"]
+    REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.email
