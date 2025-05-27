e0053d209d829fc5ae24fd55a1f07b75b08596ee
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-26 21:51:54 -0300

    Atualizar endpoint de registro de usuário para utilizar um modelo de entrada personalizado e criptografar a senha antes de salvar no banco de dados.

--- myproject/myproject/api.py
+++ myproject/myproject/api.py
@@ -1,23 +1,22 @@
-from ninja import NinjaAPI, Schema
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from ninja import NinjaAPI
+
+from myproject.schemas.user_schema import UserIn, UserOut
 
 api = NinjaAPI()
 
 
-class User(Schema):
-    username: str
-    password: str
-
-
-class UserOut(Schema):
-    id: int
-    username: str
-
-
-@api.post("/register", User, response=UserOut)
-def create_user(request, data: User):
-    user = User(username=data.username, password=data.password)
-    user.save()
-    return user
+@api.post("/register", response=UserOut)
+def create_user(request, data: UserIn):
+    user = User.objects.create(
+        username=data.username,
+        first_name=data.first_name,
+        last_name=data.last_name,
+        email=data.email,
+        password=make_password(data.password),
+    )
+    return UserOut(id=user.id, username=user.username)
 
 
 @api.get("/hello")
