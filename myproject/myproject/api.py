b6b5d56faf14ea2aa05cdfd410f49dd4f03f1107
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-31 22:21:04 -0300

    Adicionar endpoints de login, obter usuário atual e listar tarefas do usuário autenticado, utilizando autenticação JWT.

--- myproject/myproject/api.py
+++ myproject/myproject/api.py
@@ -1,11 +1,19 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Task
 from ninja import NinjaAPI
+from ninja.errors import HttpError
 
-from myproject.schemas.user_schema import UserIn, UserOut
+from myproject.schemas.auth_schema import LoginIn, TokenOut
 from myproject.schemas.task_schema import TaskIn, TaskOut
+from myproject.schemas.user_schema import UserIn, UserOut
+
+from .auth_utils import generate_jwt_token
+from .middlewares.auth_middleware import JWTAuth
 
 api = NinjaAPI()
+
+jwt_auth = JWTAuth()
 
 
 @api.post("/register", response=UserOut)
@@ -20,7 +28,30 @@ def create_user(request, data: UserIn):
     return UserOut(id=user.id, username=user.username)
 
 
+@api.post("/login", response=TokenOut)
+def login(request, data: LoginIn):
+    user = authenticate(username=data.username, password=data.password)
+    if user:
+        token = generate_jwt_token(user.id, user.username)
+        return TokenOut(access_token=token, user_id=user.id, username=user.username)
+
+    raise HttpError(401, "Credenciais inválidas")
+
+
 @api.get("/get_users", response=list[UserOut])
 def get_users(request):
     users = User.objects.all()
     return users
+
+
+@api.get("/me", response=UserOut, auth=jwt_auth)
+def get_current_user(request):
+    user = request.auth
+    return UserOut(id=user.id, username=user.username)
+
+
+@api.get("/my_tasks", response=list[TaskOut], auth=jwt_auth)
+def get_my_tasks(request):
+    user = request.auth
+    tasks = Task.objects.filter(user=user)
+    return tasks
