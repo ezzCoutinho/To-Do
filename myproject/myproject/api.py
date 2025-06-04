02e6ed3b0d031d1d09b668a0da71a49291818ca2
ezzcoutinho <lammerlopes@hotmail.com>
2025-06-03 22:33:28 -0300

    Adicionar decorador de tratamento de erros nas rotas da API, substituindo exceções por respostas HTTP apropriadas, incluindo autenticação e manipulação de tarefas.

--- myproject/myproject/api.py
+++ myproject/myproject/api.py
@@ -2,10 +2,12 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from ninja import NinjaAPI
-from ninja.errors import HttpError
 
+from myproject.errors.error_decorator import handle_api_errors
+from myproject.errors.types.http_not_found import HttpNotFound
+from myproject.errors.types.http_unauthorized import HttpUnauthorized
 from myproject.schemas.auth_schema import LoginIn, TokenOut
-from myproject.schemas.task_schema import TaskCreateIn, TaskIn, TaskOut, TaskUpdateIn
+from myproject.schemas.task_schema import TaskCreateIn, TaskOut, TaskUpdateIn
 from myproject.schemas.user_schema import UserIn, UserOut
 from tasks.models import Task
 
@@ -18,6 +20,7 @@ jwt_auth = JWTAuth()
 
 
 @api.post("/register", response=UserOut)
+@handle_api_errors
 def create_user(request, data: UserIn):
     user = User.objects.create(
         username=data.username,
@@ -30,28 +33,32 @@ def create_user(request, data: UserIn):
 
 
 @api.post("/login", response=TokenOut)
+@handle_api_errors
 def login(request, data: LoginIn):
     user = authenticate(username=data.username, password=data.password)
     if user:
         token = generate_jwt_token(user.id, user.username)
         return TokenOut(access_token=token, user_id=user.id, username=user.username)
-
-    raise HttpError(401, "Credenciais inválidas")
+    else:
+        raise HttpUnauthorized("Credenciais inválidas")
 
 
 @api.get("/get_users", response=list[UserOut])
+@handle_api_errors
 def get_users(request):
     users = User.objects.all()
     return users
 
 
 @api.get("/me", response=UserOut, auth=jwt_auth)
+@handle_api_errors
 def get_current_user(request):
     user = request.auth
     return UserOut(id=user.id, username=user.username)
 
 
 @api.post("/tasks", response=TaskOut, auth=jwt_auth)
+@handle_api_errors
 def create_task(request, data: TaskCreateIn):
     user = request.auth
     task = Task.objects.create(
@@ -75,6 +82,7 @@ def create_task(request, data: TaskCreateIn):
 
 
 @api.get("/my_tasks", response=list[TaskOut], auth=jwt_auth)
+@handle_api_errors
 def get_my_tasks(request):
     user = request.auth
     tasks = Task.objects.filter(author=user)
@@ -95,6 +103,7 @@ def get_my_tasks(request):
 
 
 @api.get("/tasks", response=list[TaskOut], auth=jwt_auth)
+@handle_api_errors
 def get_all_tasks(request):
     tasks = Task.objects.all()
     return [
@@ -114,22 +123,19 @@ def get_all_tasks(request):
 
 
 @api.put("/tasks/{task_id}", response=TaskOut, auth=jwt_auth)
+@handle_api_errors
 def update_task(request, task_id: int, data: TaskUpdateIn):
     user = request.auth
     try:
         task = Task.objects.get(id=task_id, author=user)
     except Task.DoesNotExist:
-        raise HttpError(
-            404, "Tarefa não encontrada ou você não tem permissão para editá-la"
-        )
+        raise HttpNotFound("Tarefa não encontrada.")
 
     task.title = data.title
     task.description = data.description
     task.status = data.status
     task.due_date = data.due_date
-
     task.completed = data.status == "done"
-
     task.save()
 
     return TaskOut(
@@ -146,14 +152,13 @@ def update_task(request, task_id: int, data: TaskUpdateIn):
 
 
 @api.delete("/tasks/{task_id}", auth=jwt_auth)
+@handle_api_errors
 def delete_task(request, task_id: int):
     user = request.auth
     try:
         task = Task.objects.get(id=task_id, author=user)
     except Task.DoesNotExist:
-        raise HttpError(
-            404, "Tarefa não encontrada ou você não tem permissão para deletá-la"
-        )
+        raise HttpNotFound("Tarefa não encontrada.")
 
     task.delete()
     return {"message": "Tarefa deletada com sucesso"}
